166934ca3fb27cddda101356cc55de6235d62820
GlowBlood <ipberezovskaya@yandex.ru>
2017-09-25 23:24:40 +0300

    Попробовала сделать тесты, пока не получается

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,12 @@
-public class Main {
+import java.util.Arrays;
+import java.util.List;
 
-   public static void main(){
-       int a[] = {1, 5, 2, 8, -4, 0};
-       Choice.choose(a);
-       prn(a);
-
-    }
-
-   private static void prn(int a[]){
-       for (int anA : a) System.out.print(anA + " ");
-        System.out.println();
-    }
-}
 
 class Choice{
-    static int[] choose(int a[]) {
+
+    static List choose(int a[]) {
         int N = a.length;
-        int maxSum = 0;
+        int maxSum = a[0];
         int sum = 0;
         int k = 0;
         int leftIndex = 0;
@@ -37,7 +27,10 @@ class Choice{
             }
         }
 
-        return new int[]{maxSum, leftIndex, rightIndex};
+        int b[] = new int[]{maxSum, leftIndex, rightIndex};
+        List list = Arrays.asList(b);
+        return list;
     }
+
 }
 
